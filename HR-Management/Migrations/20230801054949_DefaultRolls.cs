2c8f4a3c1031b649c02bd94995a91f8fe9a701a3
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2023-08-01 02:03:04 -0400

    DefaultRolls

---
+++ HR-Management/Migrations/20230801054949_DefaultRolls.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HR_Management.Migrations
+{
+    /// <inheritdoc />
+    public partial class DefaultRolls : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Rolls (Name, PeriodMoths , Augment) VALUES ('Worker', 5, 3)");
+            migrationBuilder.Sql("INSERT INTO Rolls (Name, PeriodMoths , Augment) VALUES ('Specialists', 8, 3)");
+            migrationBuilder.Sql("INSERT INTO Rolls (Name, PeriodMoths , Augment) VALUES ('Managers', 12, 3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Rolls WHERE Name = 'Worker'");
+            migrationBuilder.Sql("DELETE FROM Rolls WHERE Name = 'Specialists'");
+            migrationBuilder.Sql("DELETE FROM Rolls WHERE Name = 'Managers'");
+        }
+    }
+}
